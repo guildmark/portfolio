f14e0acba6d134716742458f29c801e286064bf3
guildmark <guildmark@gmail.com>
2020-06-19 12:16:59 +0200

    added profile picture and short description

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,18 @@
 import React, {useState} from 'react';
-
+import Profile from "../images/profile.jpg";
 function About()  {
 
     return(
         <div>
-            <h2>TestAbout</h2>
+            <h2>About Me</h2>
+            <img src={Profile} alt="profile"></img>
+            <div>
+                <p> I am currently working on my Masters in Interaction Technology and Design!</p>
+            </div>
+            <div>
+                <h3>Experience</h3>
+                <p>Here you can see the things I am more or less prolific in</p>
+            </div>
         </div>
     )
 }
