29ec478baf922a5edba86e79933dd720af818f8a
guildmark <guildmark@gmail.com>
2020-06-25 14:52:46 +0200

    Added home button to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
 import Profile from "../images/profile.jpg";
+import {Link} from "react-router-dom";
 function About()  {
 
     return(
         <div>
             <h2>About Me</h2>
+            <div>
+                {/* Add MK logo to return home */}
+                <Link to="/" href="#">HOME</Link>
+            </div>
+
             <img src={Profile} alt="profile"></img>
+
             <div>
                 <p> I am currently working on my Masters in Interaction Technology and Design!</p>
             </div>
