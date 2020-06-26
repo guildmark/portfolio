01cadcb95d3fabafafb5f4961142858296e80173
guildmark <guildmark@gmail.com>
2020-06-26 15:29:16 +0200

    Added form for contact

--- src/components/About.js
+++ src/components/About.js
@@ -20,6 +20,17 @@ function About()  {
                 <h3>Experience</h3>
                 <p>Here you can see the things I am more or less prolific in</p>
             </div>
+
+
+            <div>
+                <h3>Contact</h3>
+
+                {/*Add contact form */}
+                <form>
+                    <label>Name</label>
+                    <input type="text" className="form-control"/>
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,14 @@
 import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 function Work(){
     return(
         <div>
             <h2>TestWork</h2>
+            <div>
+                {/* Add MK logo to return home */}
+                <Link to="/" href="#">HOME</Link>
+            </div>
         </div>
     )
 }
