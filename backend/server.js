b11302c4cd3395c7863cef79d398b51a3bf36db9
guildmark <guildmark@gmail.com>
2020-07-10 13:17:11 +0200

    created express server to use for mail form

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Start server and listen to port
+app.listen(port, () => {
+    console.log(`Server is connected to port  + ${port}`)
+});
+
+//Send GET request
+app.get("/express_backend", (req, res) => {
+    res.send({ express: "Your express backend is connected to react"});
+})
